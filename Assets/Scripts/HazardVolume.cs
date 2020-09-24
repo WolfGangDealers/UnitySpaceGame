1dd675b471ed8417564897de7d1975f86e0b81dc
John Butler <jbutler11300@gmail.com>
2020-09-23 21:29:05 -0500

    ProjectTransfer

---
+++ Assets/Scripts/HazardVolume.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardVolume : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // detect if it's the player
+        PlayerShip playerShip
+            = other.gameObject.GetComponent<PlayerShip>();
+        
+        // if we found something valid, continue
+        if (playerShip != null)
+        {
+            // Destroy player
+            playerShip.Kill();
+        }
+    }
+}
